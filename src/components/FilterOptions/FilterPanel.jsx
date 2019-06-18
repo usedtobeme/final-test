29de175233087631d8b404d270ca755cbe708aca
Nicolas <nicolas.simmonds@endava.com>
2019-06-17 23:11:56 -0500

    side bare finished, all functionalities compleated, missing responsive design
     modal now appear on main screen when clicked on add klatsch button
    
    added gender filed to sidebar adn now generates a random avatar according to gender
    
    finished form, now opens modal when appointment clicked
    
    side bar almost finished, little ug when clicking confirm button
    
    finished all functionalities, missing responsive

--- src/components/FilterOptions/FilterPanel.jsx
+++ src/components/FilterOptions/FilterPanel.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./FilterPanelStyle.scss";
 import Appointments from "../Appointments/Appointments";
+import Sidebar from "../SideBar/SideBar";
 
 const state = {
   All: true,
@@ -11,100 +12,140 @@ const state = {
 
 export default function FilteredPanel() {
   const [selected, setSelected] = useState(state);
+  const [show, setShow] = useState(false);
+  const [id, setId] = useState();
+  const [edit, setEdit] = useState(false);
 
   useEffect(() => {
-    const numberOfSelectedItems = Object.entries(selected).filter(
-      e => e[0] !== "All" && e[1]
-    );
-      
-    if (numberOfSelectedItems.length === 3||numberOfSelectedItems.length === 0) setSelected(state);
+    const items = Object.entries(selected).filter(e => e[0] !== "All" && e[1]);
+
+    if (items.length === 3 || items.length === 0) setSelected(state);
   }, [selected]);
 
+  useEffect(() => {}, [edit]);
+
+  const renderModal = () => {
+    return show ? <Sidebar close={close} id={id} edit={edit} /> : null;
+  };
+
+  const close = () => {
+    console.log("a");
+    setShow(!show);
+    setEdit(false);
+    setId(undefined);
+  };
+
+  const getId = e => {
+    console.log("b");
+
+    setId(e);
+    setShow(!show);
+  };
+
+  const changeEdit = () => {
+    console.log("c");
+
+    setEdit(!edit);
+    setShow(!show);
+  };
+
   return (
-    <section className='section-filters'>
-      <div className='combobox-container' name='select-filters'>
-        <span className='show-only'>Show only:</span>
-        <ul aria-label='filter-options' className='filter-options'>
-          <li className='list-item'>
-            <button
-              className={
-                selected.All
-                  ? "button list-item__button"
-                  : "button list-item__button-deactivated"
-              }
-              value='All'
-              onClick={() => setSelected(state)}
-              aria-label='show all dates'>
-              All
-            </button>
-          </li>
-          <li className='list-item' value='confirmed'>
-            <button
-              className={
-                selected.confirmed
-                  ? "button list-item__button"
-                  : "button list-item__button-deactivated"
-              }
-              value='Confirmed'
-              onClick={() =>
-                setSelected({
-                  ...selected,
-                  confirmed: !selected.confirmed,
-                  All: false
-                })
-              }
-              aria-label='filter confirmed dates'>
-              Confirmed
-              <i className='filter fas fa-check' />
-            </button>
-          </li>
-          <li className='list-item'>
-            <button
-              className={
-                selected.pending
-                  ? "button list-item__button"
-                  : "button list-item__button-deactivated"
-              }
-              value='p'
-              onClick={() =>
-                setSelected({
-                  ...selected,
-                  pending: !selected.pending,
-                  All: false
-                })
-              }
-              aria-label='filter pending dates'>
-              Pending
-              <i className='far fa-clock' />
-            </button>
-          </li>
-          <li className='list-item'>
-            <button
-              className={
-                selected.cancelled
-                  ? "button list-item__button"
-                  : "button list-item__button-deactivated"
-              }
-              value='Cancelled'
-              onClick={() =>
-                setSelected({
-                  ...selected,
-                  cancelled: !selected.cancelled,
-                  All: false
-                })
-              }
-              aria-label='filter canceled dates'>
-              Cancelled
-              <i className='fa fa-times' />
-            </button>
-          </li>
-        </ul>
-      </div>
-      <button className='addDatebtn'>
-        <i className='fas fa-coffee' />
-        Add Klatsch
-      </button>
-      <Appointments filter={selected}/>
-    </section>
+    <div aria-label='modal-container'>
+      <section className='section-filters'>
+        <div className='combobox-container' name='select-filters'>
+          <span className='show-only'>Show only:</span>
+          <ul aria-label='filter-options' className='filter-options'>
+            <li className='list-item'>
+              <button
+                className={
+                  selected.All
+                    ? "button list-item__button"
+                    : "button list-item__button-deactivated"
+                }
+                value='All'
+                onClick={() => setSelected(state)}
+                aria-label='show all dates'>
+                All
+              </button>
+            </li>
+            <li className='list-item' value='confirmed'>
+              <button
+                className={
+                  selected.confirmed
+                    ? "button list-item__button"
+                    : "button list-item__button-deactivated"
+                }
+                value='Confirmed'
+                onClick={() =>
+                  setSelected({
+                    ...selected,
+                    confirmed: !selected.confirmed,
+                    All: false
+                  })
+                }
+                aria-label='filter confirmed dates'>
+                Confirmed
+                <i className='filter fas fa-check' />
+              </button>
+            </li>
+            <li className='list-item'>
+              <button
+                className={
+                  selected.pending
+                    ? "button list-item__button"
+                    : "button list-item__button-deactivated"
+                }
+                value='p'
+                onClick={() =>
+                  setSelected({
+                    ...selected,
+                    pending: !selected.pending,
+                    All: false
+                  })
+                }
+                aria-label='filter pending dates'>
+                Pending
+                <i className='far fa-clock' />
+              </button>
+            </li>
+            <li className='list-item'>
+              <button
+                className={
+                  selected.cancelled
+                    ? "button list-item__button"
+                    : "button list-item__button-deactivated"
+                }
+                value='Cancelled'
+                onClick={() =>
+                  setSelected({
+                    ...selected,
+                    cancelled: !selected.cancelled,
+                    All: false
+                  })
+                }
+                aria-label='filter canceled dates'>
+                Cancelled
+                <i className='fa fa-times' />
+              </button>
+            </li>
+          </ul>
+        </div>
+        <button
+          className='addDatebtn'
+          onClick={() => {
+            setShow(!show);
+          }}>
+          <i className='fas fa-coffee' />
+          Add Klatsch
+        </button>
+        <Appointments
+          filter={selected}
+          show={show}
+          getElement={getId}
+          edit={changeEdit}
+        />
+      </section>
+      {renderModal()}
+    </div>
   );
 }
--- src/components/MonthView/MonthView.jsx
+++ src/components/MonthView/MonthView.jsx
@@ -1,129 +1,129 @@
-import React, { useState, useEffect } from "react";
-import { Months } from "../../constants";
-import * as Service from "../../service/appService";
-import "./MonthView.scss";
+import React, { useState, useEffect } from 'react'
+import { Months } from '../../constants'
+import * as Service from '../../service/appService'
+import './MonthView.scss'
 
 export default function MonthView() {
   const [currDate, setCurrDate] = useState({
     Month: new Date().getMonth(),
-    Year: new Date().getFullYear()
-  });
+    Year: new Date().getFullYear(),
+  })
 
-  const [users, setUsers] = useState();
+  const [users, setUsers] = useState()
   useEffect(() => {
     Service.getAll(e => {
-      setUsers(e.data);
-    });
-  }, []);
+      setUsers(e.data)
+    })
+  }, [])
 
   const checkDateRange = e => {
     return (
       currDate.Month === new Date(e.appointment.start).getMonth() &&
       currDate.Year === new Date(e.appointment.start).getFullYear()
-    );
-  };
+    )
+  }
   const getConfirmed = () => {
     if (users)
-      return users.filter(e => e.status === "confirmed" && checkDateRange(e))
-        .length;
-  };
+      return users.filter(e => e.status === 'confirmed' && checkDateRange(e))
+        .length
+  }
   const getPending = () => {
     if (users)
-      return users.filter(e => e.status === "pending" && checkDateRange(e))
-        .length;
-  };
+      return users.filter(e => e.status === 'pending' && checkDateRange(e))
+        .length
+  }
   const getCancelled = () => {
     if (users)
-      return users.filter(e => e.status === "cancelled" && checkDateRange(e))
-        .length;
-  };
+      return users.filter(e => e.status === 'cancelled' && checkDateRange(e))
+        .length
+  }
 
   function setMonthView(e) {
     if (e === -1) {
       if (currDate.Month === 0 && currDate.Year > 2019) {
         setCurrDate({
           Month: 11,
-          Year: currDate.Year - 1
-        });
+          Year: currDate.Year - 1,
+        })
       } else if (
         currDate.Month > new Date().getMonth() ||
         currDate.Year > new Date().getFullYear()
       )
         setCurrDate({
           ...currDate,
-          Month: currDate.Month - 1
-        });
+          Month: currDate.Month - 1,
+        })
     } else {
       if (currDate.Month === 11) {
         setCurrDate({
           Month: 0,
-          Year: currDate.Year + 1
-        });
+          Year: currDate.Year + 1,
+        })
       } else
         setCurrDate({
           ...currDate,
-          Month: currDate.Month + 1
-        });
+          Month: currDate.Month + 1,
+        })
     }
   }
 
   const keyDown = (month, e) => {
-    if (e.key === "Enter") {
-      setMonthView(month);
+    if (e.key === 'Enter') {
+      setMonthView(month)
     }
-  };
+  }
 
   return (
-    <section className='section-sidebar'>
-      <section className='section-date'>
+    <section className="section-sidebar">
+      <section className="section-date">
         <i
-          className='fas fa-chevron-left'
+          className="fas fa-chevron-left"
           onClick={() => setMonthView(-1)}
           onKeyDown={e => keyDown(-1, e)}
           tabIndex={1}
-          aria-label='previous month view'
+          aria-label="previous month view"
         />
-        <div className='section-date__container'>
-          <span className='section-date__month'>{Months[currDate.Month]}</span>
-          <span className='section-date__year'>{currDate.Year}</span>
+        <div className="section-date__container">
+          <span className="section-date__month">{Months[currDate.Month]}</span>
+          <span className="section-date__year">{currDate.Year}</span>
         </div>
         <i
-          className='fas fa-chevron-right'
+          className="fas fa-chevron-right"
           onClick={() => setMonthView(1)}
           tabIndex={2}
           onKeyDown={e => keyDown(1, e)}
-          aria-label='next month view'
+          aria-label="next month view"
         />
       </section>
-      <section className='section-summary'>
-        <div className='section-summary__container'>
-          <div className='section-summary__icon section-summary__icon__confirmed'>
-            <i className='icon fas fa-check' />
+      <section className="section-summary">
+        <div className="section-summary__container">
+          <div className="section-summary__icon section-summary__icon__confirmed">
+            <i className="icon fas fa-check" />
           </div>
-          <div className='section-summary__text'>
+          <div className="section-summary__text">
             <span>{getConfirmed()}</span>
             <span>Confirmed</span>
           </div>
         </div>
-        <div className='section-summary__container'>
-          <div className='section-summary__icon section-summary__icon__pending'>
-            <i className='icon far fa-clock' />
+        <div className="section-summary__container">
+          <div className="section-summary__icon section-summary__icon__pending">
+            <i className="icon far fa-clock" />
           </div>
-          <div className='section-summary__text'>
+          <div className="section-summary__text">
             <span>{getPending()}</span>
             <span>Pending</span>
           </div>
         </div>
-        <div className='section-summary__container'>
-          <div className='section-summary__icon section-summary__icon__canceled'>
-            <i className='icon fas fa-times' />
+        <div className="section-summary__container">
+          <div className="section-summary__icon section-summary__icon__canceled">
+            <i className="icon fas fa-times" />
           </div>
-          <div className='section-summary__text'>
+          <div className="section-summary__text">
             <span>{getCancelled()}</span>
             <span>Cancelled</span>
           </div>
         </div>
       </section>
     </section>
-  );
+  )
 }
